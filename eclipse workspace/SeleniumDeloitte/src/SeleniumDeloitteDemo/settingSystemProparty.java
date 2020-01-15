e8cbb7afa9a92626951bd80401945701c115f49e
prasannabhat97 <37222040+prasannabhat97@users.noreply.github.com>
2020-01-15 17:00:19 +0530

    Add files via upload

--- eclipse workspace/SeleniumDeloitte/src/SeleniumDeloitteDemo/settingSystemProparty.java
+++ eclipse workspace/SeleniumDeloitte/src/SeleniumDeloitteDemo/settingSystemProparty.java
@@ -9,14 +9,16 @@ public class settingSystemProparty {
 		
 		System.setProperty("webdriver.chrome.driver", "E://eclipse//chromedriver_win32/chromedriver.exe");
 		WebDriver driver=new ChromeDriver();
-		driver.get("https://google.com");
+		//driver.get("https://google.com");
 		driver.manage().window().maximize();
 		//driver.findElement(By.name("q")).sendKeys("online fps games");
 		driver.get("https://www.linkedin.com/login");
 		driver.findElement(By.name("session_key")).sendKeys("prasannabhat97@gmail.com");
 		driver.findElement(By.name("session_password")).sendKeys("kkkkkkkkkkkk");
-		
-	//	driver.close();	
+		driver.findElement(By.cssSelector("#app__container > main > div > form > div.login__form_action_container > button")).click();
+		driver.findElement(By.xpath("/html/body/div/main/div/div[3]/div/a")).click();
+	
+		//	driver.close();	
 	}
 
 }
